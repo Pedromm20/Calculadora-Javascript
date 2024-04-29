77da6104539b7f69fd45e342acab4685fb4b7d8f
Pedromm20 <153853545+Pedromm20@users.noreply.github.com>
2024-04-29 16:23:44 +0200

    v.2 calculadora con funciones nuevas

--- calculadora.js
+++ calculadora.js
@@ -16,12 +16,54 @@ function calcular() {
     }
 }
 
+function calcularPorcentaje() {
+    try {
+        let valor = eval(document.getElementById("screen").value);
+        let resultado = valor / 100;
+        document.getElementById("screen").value = resultado;
+    } catch (error) {
+        alert('Error en la expresión');
+        limpiarPantalla();
+    }
+}
+
+function calcularPotencia(exponente) {
+    try {
+        let valor = eval(document.getElementById("screen").value);
+        let resultado = Math.pow(valor, exponente);
+        document.getElementById("screen").value = resultado;
+    } catch (error) {
+        alert('Error en la expresión');
+        limpiarPantalla();
+    }
+}
+
+function calcularRaizCuadrada() {
+    try {
+        let valor = eval(document.getElementById("screen").value);
+        if (valor >= 0) {
+            let resultado = Math.sqrt(valor);
+            document.getElementById("screen").value = resultado;
+        } else {
+            alert('No se puede calcular la raíz cuadrada de un número negativo');
+            limpiarPantalla();
+        }
+    } catch (error) {
+        alert('Error en la expresión');
+        limpiarPantalla();
+    }
+}
+
+function borrarDigito() {
+    let pantalla = document.getElementById("screen");
+    pantalla.value = pantalla.value.slice(0, -1);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const btnTema = document.querySelector('.theme-btn');
     const btnReset = document.querySelector('.clear');
     const btnEquals = document.querySelector('.equals');
 
-    // Función para cambiar entre temas claro y oscuro
     btnTema.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme');
         btnReset.classList.toggle('dark-theme');
